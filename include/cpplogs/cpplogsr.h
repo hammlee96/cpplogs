d2bede7c6d60df45e6f987b29af795d5f7729d6c
hammlee <hammlee@163.com>
2023-12-28 21:08:40 +0800

    add read log file function

--- include/cpplogs/cpplogsr.h
+++ include/cpplogs/cpplogsr.h
@@ -13,12 +13,28 @@
 #include "fileformat.h"
 
 namespace CppLogs {
-	class CPPLOGS_API CppLogsR 
+	class CPPLOGS_API CppLogsR : public FileFormat
 	{
 	public:
-		CppLogsR(const std::string& filename) : public FileFormat(filename)
+		CppLogsR(const std::string& filename) : FileFormat(filename)
 		{}
 		~CppLogsR()
 		{}
+
+		Error::EnErrorCode get_item_type(FileFormat::StCppLogsHeader& st_CppLogsItem)
+		{
+			if (!existfile()) {
+				return Error::ErrorCode_OpenFileFailed;
+			}
+			return st_CppLogsItem = unformat_header();
+		}
+
+		Error::EnErrorCode get_items(std::vector<FileFormat::StCppLogsItem>& st_CppLogsItemVector)
+		{
+			if (!existfile()) {
+				return Error::ErrorCode_OpenFileFailed;
+			}
+			return unformat_data(st_CppLogsItemVector);
+		}
 	};
 }
