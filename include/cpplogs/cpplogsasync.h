d9cf320743b9759fecd662d660a088aa2e7ea6c9
hammlee <hammlee@163.com>
2024-01-04 18:56:56 +0800

    add comparePlus module; add CppLogsASync class

---
+++ include/cpplogs/cpplogsasync.h
@@ -0,0 +1,22 @@
+/*
+* function		: sync logs
+* author		: hammlee
+* e-mail		: hammlee@163.com
+* create date	: 2024.01.04
+*
+* change author	: hammlee
+* change date	: 2024.01.04
+* change log	: init
+*/
+
+#pragma once
+
+#include "base.h"
+namespace {
+	class CPPLOGS_API CppLogsASync
+	{
+	public:
+		CppLogsSync(){}
+		~CppLogsSync(){}
+	};
+}
