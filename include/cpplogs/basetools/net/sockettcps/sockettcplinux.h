05d86cc60430dd9346f8937eeb3bde5337262aa4
hammlee <hammlee@163.com>
2024-01-30 12:55:02 +0800

    Adapt to Linux TCP

--- include/cpplogs/basetools/net/sockettcps/sockettcplinux.h
+++ include/cpplogs/basetools/net/sockettcps/sockettcplinux.h
@@ -13,16 +13,20 @@
 #pragma once
 
 #include "sockettcpbase.h"
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 namespace CppLogs
 {
 	class SocketTcpLinuxClient : public SocketTcpBase
 	{
 	public:
-		SocketTcpLinuxClient(const std::string& hostip = "127.0.0.1", const int& hostport = 9605)
+		SocketTcpLinuxClient(const std::string& hostip = "127.0.0.1", const int& hostport = 9605) : 
+			_hostip(hostip), _hostport(hostport)
 		{
-
+			_socket = -1;
 		}
 		~SocketTcpLinuxClient()
 		{
@@ -36,28 +40,53 @@ namespace CppLogs
 
 		Error::EnCppLogsNetError connect() override
 		{
+			int _socket = ::socket(AF_INET, SOCK_STREAM, 0);
+			if (_socket == -1) {
+				return Error::EnCppLogsNetError_InitFailed;
+			}
+			struct ::sockaddr_in st_sockaddr_in;
+			st_sockaddr_in.sin_family = AF_INET;
+			st_sockaddr_in.sin_addr.s_addr = inet_addr(_hostip.c_str());
+			st_sockaddr_in.sin_port = _hostport;
+			if (::connect(_socket, (struct sockaddr*)&st_sockaddr_in, sizeof(st_sockaddr_in)) == -1) {
+				return Error::EnCppLogsNetError_ConnectFailed;
+			}
+
 			return Error::EnCppLogsNetError_None;
 		}
 
 		Error::EnCppLogsNetError disconnect() override
 		{
+			::close(_socket);
+			_socket = -1;
 			return Error::EnCppLogsNetError_None;
 		}
 
 		Error::EnCppLogsNetError send(const char* data, const size_t& size) override
 		{
+			if (::send(_socket, data, size, 0) != size) {
+				return Error::EnCppLogsNetError_SendFailed;
+			}
 			return Error::EnCppLogsNetError_None;
 		}
 
 		Error::EnCppLogsNetError recv(char* data, size_t& size) override
 		{
+			if (::recv(_socket, data, size, 0) < 0) {
+				return Error::EnCppLogsNetError_RecvFailed;
+			}
 			return Error::EnCppLogsNetError_None;
 		}
 
 		bool is_connected() const override
 		{
-			return true;
+			return _socket != -1;
 		}
+
+	private:
+		std::string _hostip;
+		int _hostport;
+		int _socket;
 	};
 
 	class SocketTcpLinuxServer : public SocketTcpBase
